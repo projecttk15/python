5136e1dce8fb26a5f2a59d2cfb7dd8dff882c423
Quoc Thang <quocthangtk15@gmail.com>
2018-05-23 08:04:44 -0700

    string: update practice string
    Signed-off-by: Quoc Thang <quocthangtk15@gmail.com>

---
+++ test/var-string.py
@@ -0,0 +1,21 @@
+a = 0
+b = 1
+c = d = e = 2
+f = "Quoc Ngo"
+print (a, b, c, d, e, f)
+print ("first value", f[0])
+print ("mid", f[2:5])
+print (f + " thang")
+print ("mul", f*3)
+
+if("Q" in f):
+	print (f)
+
+print("from 2" , f[2:] , "end 2", f[:2])
+
+print ("Ten toi la %s va toi nang %d kg!", f, 71)
+
+para_str = """day la mot chuoi day gom nhieu dong 
+va gom mot so ky tu khong in duoc chang han nhu
+TAB ( \t ) chung se duoc hien thi dung nguyen van nhu the."""
+print (para_str)
--- test/variable.py
+++
@@ -1,5 +0,0 @@
-a = 0
-b = 1
-c = d = e = 2
-f = "Quoc Ngo"
-print (a, b, c, d, e, f)
