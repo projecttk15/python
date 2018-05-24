feab9391999718447c49055736d1e7dc2b44cdf9
Quoc Thang <quocthangtk15@gmail.com>
2018-05-24 07:15:58 -0700

    list: update  list practice
    Signed-off-by: Quoc Thang <quocthangtk15@gmail.com>

---
+++ test/list.py
@@ -0,0 +1,11 @@
+list1 = ['vatly', 'hoahoc', 1997, 2000];
+list2 = [1, 2, 3, 4, 5 ];
+list3 = ["a", "b", "c", "d"];
+print (list1)
+print (list1 + list2)
+#add to end list
+list1.append('quoc')
+print ("add to list", list1)
+#delete 1 var
+del list1[1]
+print ("del in list", list1)
